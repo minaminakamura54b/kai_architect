418c720b1a4ad8bccd03239777e93c3e4f55ba3e
Minami Nakamura <nakamura0373@gmail.com>
2026-02-22 22:13:07 -0800

    header done Main page controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'main_pages#home'
+  get 'main_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
